dd1754aab2dca27e50dd0762a8c196afefe0d2d9
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-04-30 17:07:57 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
 
 
 const fetchData = async () => {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	const response = await fetch("http://localhost:3000/week");
 	return await response.json();
 };
 

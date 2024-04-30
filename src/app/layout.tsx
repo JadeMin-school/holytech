e10481a6a1a36d2b1c651a78a305a6955b8ae79b
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-04-30 15:35:01 +0900

    upload: upload some files

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Holytech",
+	description: "폴리텍 정보 앱",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="ko">
+			<body>{children}</body>
+		</html>
+	);
+}
